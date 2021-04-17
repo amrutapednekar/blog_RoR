0ccf82555de483444580b35ba3c309f15c4ea57c
amrutapednekar <amruta.jadhav1@gmail.com>
2021-04-17 17:10:29 +0200

    1. Refactored comments code 2. Added dependant destory for Atricle-comments. 3. Added basic HTTP authentication.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+    http_basic_authenticate_with name: "Amruta", password: "Amruta", except: [:index, :show]
     def index
         @articles = Article.all
     end
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,19 @@
 class CommentsController < ApplicationController
+    http_basic_authenticate_with name: "Amruta", password: "Amruta", only: [:destroy]
+
     def create
         @article = Article.find(params[:article_id])
         @comment = @article.comments.create(comments_params)
         redirect_to article_path(@article)
-    end   
+    end
+
+    def destroy
+        @article = Article.find(params[:article_id])
+        @comment = @article.comments.find(params[:id])
+        @comment.destroy
+        redirect_to article_path(@article)
+    end
+
     private
     def comments_params
         params.require(:comment).permit(:commentor, :body)
